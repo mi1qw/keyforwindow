f95edc4d65fd7e6e941ddc06aca59d5c2ee0242c
mi1qw <123mib@gmail.com>
2023-04-13 02:16:20 +0300

    0

--- src/keyforwindow/keyforwindow.go
+++ src/keyforwindow/keyforwindow.go
@@ -1,8 +1,51 @@
 package main
 
-import "C"
+import (
+	"fmt"
+	"github.com/BurntSushi/xgbutil"
+	"github.com/BurntSushi/xgbutil/ewmh"
+	"github.com/BurntSushi/xgbutil/xwindow"
+	"time"
+)
 
 func main() {
+	X, err := xgbutil.NewConn()
+	if err != nil {
+		panic(err)
+	}
+	
+	for {
+		time.Sleep(500 * time.Millisecond)
 
-	println("Exit")
+		active, err := ewmh.ActiveWindowGet(X)
+		if err != nil {
+			panic(err)
+		}
+
+		name, err := ewmh.WmNameGet(X, active)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Active window:", name, active)
+
+		// Get the root window.
+		//root := X.RootWin()
+
+		// Get the ID of the active window.
+		//activeWin, err := xgbutil.GetActiveWindow(X, root)
+		//if err != nil {
+		//	panic(err)
+		//}
+
+		// Get the geometry of the active window.
+		winGeom, err := xwindow.New(X, active).Geometry()
+		if err != nil {
+			panic(err)
+		}
+
+		// Print the geometry of the active window.
+		fmt.Printf("Active window geometry: %+v\n", winGeom)
+
+	}
+
 }
